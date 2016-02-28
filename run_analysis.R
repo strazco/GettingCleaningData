bca6488a97a1cbebe6cb3d4173c59ee4b9e6788d
strazco <andy@strazco.com>
2016-02-28 15:10:51 -0600

    final run_anlaysis set to download ZIP file

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,12 @@
 
+## Andrew Strasman
+## Coursera Project Getting and Cleaning Data
+## Project Assignment
+## Feb 2016
+
+## Watch this video to understand the data that is being collected
+## https://www.youtube.com/watch?v=XOEN9W05_4A&ab_channel=JorgeLuisReyesOrtiz
+
 ## You should create one R script called run_analysis.R that does the following.
 
 ## 1. Merges the training and the test sets to create one data set.
@@ -15,18 +23,19 @@
 ##https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 
-
 # Package Requirements
 
 require("data.table")
 require("dplyr")
 
+library(data.table)
+library(dplyr)
+
 
 # download ZIP file and unpack
-#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",zfile)
-#unzip("zfile", overwrite = TRUE)
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","zfile")
+unzip("zfile", overwrite = TRUE)
 
-# load and merge training and test sets
 
 # first get real variable names and descriptive activity names
 var_names <-read.table("./UCI HAR Dataset/features.txt") [,2]
@@ -41,18 +50,17 @@ names(x_train) = var_names
 # Y vals
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 y_train[,2] = act_labels[y_train[,1]]
-names(y_train) = c("Act_ID", "Act_Label")
+names(y_train) = c("ActID", "ActLabel")
 
 # Subjects
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-names(subject_train) = "Subject"
+names(subject_train) = "SubjectID"
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
-meas_var <- grepl("mean|std", var_names)
+meas_var <- grepl("-mean|-std", var_names)
 x_train = x_train[meas_var]
 
 train_ds <- cbind(as.data.table(subject_train, keep.rownames = FALSE), y_train, x_train)
-
 
 
     ##### load testing data
@@ -64,11 +72,11 @@ names(x_test) = var_names
 # Y vals
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 y_test[,2] = act_labels[y_test[,1]]
-names(y_test) = c("Act_ID", "Act_Label")
+names(y_test) = c("ActID", "ActLabel")
 
 # Subjects
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-names(subject_test) = "Subject"
+names(subject_test) = "SubjectID"
 
 # Extract only the measurements on the mean and standard deviation for each measurement.
 meas_var <- grepl("mean|std", var_names)
@@ -77,12 +85,22 @@ x_test = x_test[meas_var]
 test_ds <- cbind(as.data.table(subject_test, keep.rownames = FALSE), y_test, x_test)
 
 
-## Merges the training and the test sets to create one data set.
+      ## Merge the training and the test sets to create one data set.
 
 combo_ds <- rbind(train_ds, test_ds)
 
-## Appropriately labels the data set with descriptive variable names.
+      ## Appropriately labels the data set with descriptive variable names.  Long, but descriptive.
+names(combo_ds)<-gsub("^f","Freq-", names(combo_ds))
+names(combo_ds)<-gsub("^t","Time-", names(combo_ds))
+names(combo_ds)<-gsub("Gyro","Gyroscope-", names(combo_ds))
+names(combo_ds)<-gsub("Mag","Magnitude-", names(combo_ds))
+names(combo_ds)<-gsub("Acc","Accelerometer-", names(combo_ds))
+
+write.table(combo_ds,"merged_ds.txt")
 
 
+      ## From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-View(combo_ds)
+calc_ds <- combo_ds[, lapply(.SD, mean), by=c("SubjectID","ActID")]
+write.table(calc_ds,"calculated_ds.txt")
+
